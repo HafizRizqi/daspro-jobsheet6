574e1c3a2e6e64d96aee274577159004be148914
HafizRizqi <HafizRizqi>
2024-10-03 01:45:05 +0700

    modifikasi program sesuai no.4

--- Pemilihan2Percobaan111.java
+++ Pemilihan2Percobaan111.java
@@ -7,9 +7,13 @@ public class Pemilihan2Percobaan111 {
         System.out.println("Masukkan Tahun: ");
         tahun = input11.nextInt();
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
-                 System.out.println("Tahun Kabisat");
-        } else
+            if ((tahun % 100) == 0)
+            if ((tahun % 400) == 0) 
+            System.out.println("Tahun Kabisat"); 
+            if ((tahun % 400) != 0)   
             System.out.println("Bukan Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
